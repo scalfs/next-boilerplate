3ce854aae62593f94dd0c8c5739553e0ce46537a
Vitor Escalfoni <vitor.hme@gmail.com>
2022-05-18 16:04:49 -0300

    chore: added styles to the main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>Advanced React</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Atom image with Advanced React written by the side"
+    />
+    <S.Title>Advanced React</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS and Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="A developer facing a screen with code"
+    />
+  </S.Wrapper>
 )
 
 export default Main
