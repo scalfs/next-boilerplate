776beb4e72f4d2d4b40161df4795870d9b944794
Vitor Escalfoni <vitor.hme@gmail.com>
2022-05-19 10:41:19 -0300

    chore: added storybooks

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,19 +1,14 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'Advanced React',
+  description = 'TypeScript, ReactJS, NextJS and Styled Components'
+}) => (
   <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Atom image with Advanced React written by the side"
-    />
-    <S.Title>Advanced React</S.Title>
-    <S.Description>
-      TypeScript, ReactJS, NextJS and Styled Components
-    </S.Description>
-    <S.Illustration
-      src="/img/hero-illustration.svg"
-      alt="A developer facing a screen with code"
-    />
+    <S.Logo src="/img/logo.svg" alt="Atom image with Advanced React written by the side" />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" alt="A developer facing a screen with code" />
   </S.Wrapper>
 )
 
