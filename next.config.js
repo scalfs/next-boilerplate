9f14fb6f9b3ae7da5f7eed562a28a4a895f3bdfd
Vitor Escalfoni <vitor.hme@gmail.com>
2022-09-29 20:59:01 -0300

    fix: next-pwa initialization fixes

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({ dest: 'public', disable: !isProd })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true
 }
 
-module.exports = withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: 'public',
-    disable: !isProd
-  }
-})
+module.exports = withPWA(nextConfig)
